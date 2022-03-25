3a079693076fd5a4d38680e76869ff4e91301921
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-03-25 18:53:23 -0500

    Inicio: Se muestra el login, Base de datos: Credenciales postgrest

---
+++ src/main/java/edu/eci/arsw/app/LiveCodingApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+@SpringBootApplication
+@ComponentScan(basePackages = {"edu.eci.arsw"})
+
+public class LiveCodingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiveCodingApplication.class, args);
+    }
+}
