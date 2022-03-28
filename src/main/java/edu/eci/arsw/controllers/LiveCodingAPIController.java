43a73ee922b47743a33a62ddbe7eeae7fda50f7d
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2022-03-28 09:03:21 -0500

    Se realizaron actualizaciones al Socket

--- src/main/java/edu/eci/arsw/controllers/LiveCodingAPIController.java
+++ src/main/java/edu/eci/arsw/controllers/LiveCodingAPIController.java
@@ -4,13 +4,16 @@ package edu.eci.arsw.controllers;
 
 import edu.eci.arsw.service.LiveCodingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/livecoding")
 public class LiveCodingAPIController {
 
+	public String idRoom;
 
     @Autowired
     LiveCodingService lcs;
@@ -21,6 +24,18 @@ public class LiveCodingAPIController {
         return ResponseEntity.ok(lcs.getAllUsers());
     }
 
-
+    @RequestMapping(value = "/saveRoomId", method=RequestMethod.POST)
+    public ResponseEntity savRoomId(@RequestBody String idRoom){
+        System.out.println("Entrando en /saveRoomId");
+        this.idRoom = idRoom; 
+        System.out.println("Dato guardado" + this.idRoom);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/getIdRoom", method=RequestMethod.GET)
+    public ResponseEntity getIdRoom(){
+        System.out.println("Entrando en /getIdRoom");
+        return ResponseEntity.ok(idRoom);
+    }
 
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,8 +2,8 @@ var app = (function () {
     _local = "http://localhost:8080";
     _external = "https://livecoding-gpv.herokuapp.com";
     var stompClient = null;
-    var _idsala;
-    var _id;
+    //var _idsala;
+    //var _id;
 
 
     var validateLogin = function(){
@@ -13,26 +13,41 @@ var app = (function () {
 
     var goToHome =  function(){
         _idsala = prompt('Digite el código de la sala: ');
-        localStorage.setItem(_idsala, _idsala);
+        alert("Id sala: " + _idsala);
+        //sessionStorage.setItem(_idsala, _idsala);
+
+        $.ajax({
+            url: "http://localhost:8080/livecoding/saveRoomId",
+            type: 'POST',
+            data: JSON.stringify(_idsala),
+            contentType: "application/json"
+        });
+        console.log("Guardando id: " + _idsala);
+
         if(_idsala.length !== 0){
-            console.log("Esta monda no sirve (antes del replace) => "+_idsala);
+            console.log("Prueba (antes del replace) => "+_idsala);
             window.location.assign(_local +"/principal.html");
-            console.log("Esta monda no sirve (despues del replace) => "+_idsala);
-            _id = localStorage.getItem(_idsala);
-            console.log("Esta monda no sirve (despues del replace y con el local) => "+_id);
+            console.log("Prueba (despues del replace) => "+_idsala);
+            _id = sessionStorage.getItem(_idsala);
+            console.log("Prueba (despues del replace y con el local) => "+_id);
             //window.location.replace(_external +"/principal.html");
         }else{
-            alert("Ponga un hp numero!!!");
+            alert("Ponga un numero!!!");
         }
     };
 
     var connectAndSubscribe = function () {
         var socket = new SockJS('/stompendpoint');
+        var _id;
         stompClient = Stomp.over(socket);
 
+        $.get("/livecoding/getIdRoom", function(data) {
+            _id = data;
+            console.log("Obteniendo id: " + _id);
+		});
         //_id = localStorage.getItem(_idsala);
-        console.log("Esta monda no sirve (en el connect con el local) => "+_id);
-        console.log("Esta monda no sirve (en el connect) => "+_idsala);
+        console.log("--Prueba (en el connect con el local) => "+_id);
+        console.log("--Prueba (en el connect) => "+_idsala);
 
         stompClient.connect({}, function (){
             stompClient.subscribe('/topic/file.'+_id, function (eventbody){
